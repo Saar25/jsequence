9689efca6862571370cc88883e0577cd435fbb0b
Saar Tako <saartako25@gmail.com>
2023-06-10 15:03:51 +0300

    created map method

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,5 +1,6 @@
 import { sequenceOf } from ".";
 import { filter } from "./filter";
+import { map } from "./map";
 
 export class Sequence<T> implements Iterable<T> {
     constructor(private readonly iterable: Iterable<T>) { }
@@ -10,6 +11,12 @@ export class Sequence<T> implements Iterable<T> {
 
     filter = (predicate: (value: T, index: number) => boolean) => {
         const iterable = filter(this, predicate);
+
+        return sequenceOf(iterable);
+    }
+
+    map = <S>(mapFn: (value: T, index: number) => S) => {
+        const iterable = map(this, mapFn);
 
         return sequenceOf(iterable);
     }
